ef10312118399ef59e1bbb3b1c529f906afcdc2d
Vageesha K M <35898323+vageeesh@users.noreply.github.com>
2026-09-21 18:53:01 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        num_1 = num_2 = None
+        for index, val in enumerate(nums):
+            if target-val in hashmap:
+                num_1 = hashmap[target-val]
+                num_2 = index
+
+            # if i not in hashmap:
+            hashmap[val] = index
+
+        return [num_1, num_2]
+        # if num_1 or num_2:
+        #     return [num_1, num_2]
+        # else:
+        #     return []
+
