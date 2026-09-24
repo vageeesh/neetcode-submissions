1edcd8c00c7fff8845ff1c961b506c9a0276fbc4
Vageesha K M <35898323+vageeesh@users.noreply.github.com>
2026-09-24 18:24:09 +0530

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashmap = {}
+
+        for i in nums:
+           hashmap[i] = (hashmap.get(i, 0)) + 1
+
+        # initialise an array with same leght as input because max possible array for frequency is completely distinct elements which is N
+        
+        # use list comprehensive only but not [[]]* N which will not work as that will create reference and modifying one will modify all
+        new_arr = [[] for i in range(len(nums))]
+        for key,freq in hashmap.items():
+            new_arr[freq-1].append(key)
+        
+        return_array = []
+        for i in range(len(new_arr)-1,-1,-1):
+            for each in new_arr[i]:
+                if len(return_array) < k:
+                    return_array.append(each)
+                else:
+                    return return_array
+
+        return return_array
